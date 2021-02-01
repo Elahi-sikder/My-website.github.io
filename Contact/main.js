de146a384a2917e929b1ea58235fc1f714d32d0f
ELAHI Sikder <77626169+Elahi-sikder@users.noreply.github.com>
2021-02-01 18:23:24 +0300

    Add files via upload

---
+++ Contact/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function (){
+  
+  
+  $('.icon').click(function (){
+    $('.list').show('slow')
+  });
+  
+  $('.closed').click(function (){
+    $('.list').hide('slow')
+  })
+})
