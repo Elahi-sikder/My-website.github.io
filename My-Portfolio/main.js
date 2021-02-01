fc22d71775edf390cd96a89dad570bea67194a7e
ELAHI Sikder <77626169+Elahi-sikder@users.noreply.github.com>
2021-02-01 18:27:28 +0300

    Add files via upload

---
+++ My-Portfolio/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  
+
+/*...... */
+  $('.icon').click(function() {
+    $('.list').show('slow')
+  });
+
+  $('.closed').click(function() {
+    $('.list').hide('slow')
+  });
+ 
+  $('.control-carousel').click(function (){
+  $('.carousel').toggle()
+}) ;
+
+
+
+$('.photo a').simpleLightbox({
+  
+});
+
+
+})
+
+
